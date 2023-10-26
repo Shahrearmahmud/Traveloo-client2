3caa476ed4003171241449cdaceaecb6b7799d77
Shahrear Mahmud <aodriteosrk10252@gmail.com>
2023-10-26 14:58:46 +0600

    someChanges

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:4000/api/v1'
+// export const BASE_URL = 'http://localhost:4000/api/v1'
+export const BASE_URL = 'https://traveloo-server1.onrender.com/api/v1'
